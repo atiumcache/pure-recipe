799ee6ece7ca1e8194025f7df5c81698c82eda6a
atiumcache <aattilio@pm.me>
2023-11-27 09:48:00 -0700

    add options for viewing or downloading

--- recipe-to-markdown.py
+++ recipe-to-markdown.py
@@ -6,7 +6,11 @@ recipe_url = sys.argv[1]
 
 console = Console()
 
-def main(url):
+def save_to_markdown():
+    pass
+
+
+def view_in_terminal(url):
     """
     
     :param url: a url string from a recipe website
@@ -14,8 +18,21 @@ def main(url):
     :rtype:
     """
     scraper = scrape_me(url)
-    console.print('\n\n', scraper.title(), style="bold white")
-    for ingredient in scraper.ingredients():
-        console.print('-', ingredient, style='green')
+    console.print('\n\n', scraper.title(), style="bold white", justify='center')
+    console.print('\nINGREDIENTS', style="bold white")
+    for index, ingredient in enumerate(scraper.ingredients()):
+        console.print('-', ingredient, style='gold3')
+    console.print('\nINSTRUCTIONS', style="bold white")
+    for index, instruction in enumerate(scraper.instructions_list()):
+        console.print(index+1, ') ', style='white', sep='', end='', highlight=False)
+        console.print(instruction, style='gold3')
+
+
+def main(url):
+
+    
+    view_in_terminal(url)
+    
 
 main(recipe_url)
+
