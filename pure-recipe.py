174644da772cb97e61dfc50a1eea018e7f662a32
atiumcache <aattilio@pm.me>
2023-12-22 15:45:06 -0700

    added browse, reworking view

--- pure-recipe.py
+++ pure-recipe.py
@@ -1,17 +1,25 @@
 from recipe_scrapers import scrape_me
 from rich.console import Console
+from rich.markdown import Markdown
 import rich.text
 import argparse
 import yaml
 import os
 import platformdirs
+import tempfile
 
 console = Console()
 
+
+"""
+Loads settings from yaml config file.
+Prints message if no config file.
+"""
 try:
-        with open("config.yaml", "r") as file:
-            settings = yaml.safe_load(file)
+    with open("config.yaml", "r") as file:
+        settings = yaml.safe_load(file)
 except:
+    # Config file automatically created in try block.
     print("A config.yaml file has been created in ~/.config/pure-recipe.")
     print("Please add a path to the config file to save your recipes.")
     print("No directory needed if viewing recipes in the terminal.")
@@ -65,6 +73,12 @@ def save_to_markdown(recipe_url):
     return True
 
 
+def print_markdown(md):
+    console.print('\n')
+    console.print(md)
+    console.print('\n')
+
+
 def view_in_terminal(recipe_url):
     """
     Scrapes recipe url and returns plain-text recipe to terminal output.
@@ -74,19 +88,29 @@ def view_in_terminal(recipe_url):
     :return: True if successful, False otherwise.
     """
     scraper = scrape_me(recipe_url)
+    
+    title = scraper.title()
+  
+    ### Fix
+    ### Create temp file, utilize save_to_markdown, then print to markdown
+    
 
-    console.print("\n\n", scraper.title(), style="bold white", justify="center")
 
-    console.print("\nINGREDIENTS", style="bold white")
-    for index, ingredient in enumerate(scraper.ingredients()):
-        console.print("-", ingredient, style="gold3")
+def browse_recipes():
+    with open("config.yaml", "r") as file:
+            settings = yaml.safe_load(file)
 
-    console.print("\nINSTRUCTIONS", style="bold white")
-    for index, instruction in enumerate(scraper.instructions_list()):
-        console.print(index + 1, ") ", style="white", sep="", end="", highlight=False)
-        rich.text.wrap(console.print(instruction, style="gold3"))
+    directory = os.fsencode(settings.get("directory"))
 
-    return True
+    directory = "/home/andrew/Documents/recipes/"
+
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        file_path = directory + filename
+        if filename.endswith(".md") or filename.endswith(".py"):
+            f = open(file_path, "r")
+            md = Markdown(f.read())
+            print_markdown(md)
 
 
 def load_config():
@@ -114,13 +138,15 @@ def main():
     Flow for the application. 
 
     Depending on the arguments, we either view or save the corresponding recipe. 
+
+    Or, we can browse previously saved recipes.
     """
 
     parser = argparse.ArgumentParser(
         prog="Pure Recipe", description="Make recipes pretty again."
     )
 
-    parser.add_argument("operations", choices=["view", "save", "list"])
+    parser.add_argument("operations", choices=["view", "save", "list", "browse"])
     parser.add_argument("url")
 
     args = parser.parse_args()
@@ -152,6 +178,9 @@ def main():
                 save_to_markdown(single_url)
             except:
                 console.print('\nFile error. Try again using proper file format. See documentation.\n', style="bright_red")
+
+    if args.operations == "browse":
+        browse_recipes()
 
 
 main()
