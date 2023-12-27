78167d7684880784e0e730f259a64152a1e07ae2
atiumcache <aattilio@pm.me>
2023-12-27 09:07:24 -0700

    add tests

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
 from pure_recipe import main, format_file_name, save_to_markdown, view_in_terminal, load_config
 
+
+def test_format_file_name():
+    recipe_title = "Chocolate Chip Cookies"
+    assert format_file_name(recipe_title) == "chocolate-chip-cookies"
+
+
